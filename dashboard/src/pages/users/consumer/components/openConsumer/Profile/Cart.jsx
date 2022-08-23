a950025355c99335fb8003a5292538c511abc8bc
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-08-23 11:53:41 +0600

    add: cart info in consumer profile

--- dashboard/src/pages/users/consumer/components/openConsumer/Profile/Cart.jsx
+++ dashboard/src/pages/users/consumer/components/openConsumer/Profile/Cart.jsx
@@ -4,42 +4,51 @@ import { Card, Stack, Typography, Divider } from "@mui/material";
 //util
 import { fNumber } from "../../../../../../utils/formatNumber";
 import { useGetCartQuery } from "../../../../../../store/redux/api/cart";
+import CartCard from "./CartCard";
 
 Cart.propTypes = {
   id: PropTypes.string,
 };
 
 export default function Cart({ id }) {
-  const { follower = 1900, react = 10203 } = {};
-
-  const { data } = useGetCartQuery(id);
+  const { isSuccess, data } = useGetCartQuery(id);
   console.log(data);
-  console.log(id);
   return (
     <Card sx={{ py: 3 }}>
-      <Stack
-        direction="row"
-        divider={<Divider orientation="vertical" flexItem />}
-      >
-        <Stack width={1} textAlign="center">
-          <Typography variant="h4">{fNumber(follower)}</Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Comment
-          </Typography>
-        </Stack>
+      {isSuccess && (
+        <Stack
+          direction="row"
+          divider={<Divider orientation="vertical" flexItem />}
+        >
+          <Stack width={1} textAlign="center">
+            <Typography variant="h4">{fNumber(data.subTotal)}$</Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Cart: Subtotal
+            </Typography>
+          </Stack>
+          <Stack width={1} textAlign="center">
+            <Typography variant="h4">{fNumber(data.discount)}$</Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Cart: Discount
+            </Typography>
+          </Stack>
+          <Stack width={1} textAlign="center">
+            <Typography variant="h4">{fNumber(data.total)}$</Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Cart: Total
+            </Typography>
+          </Stack>
 
-        <Stack width={1} textAlign="center">
-          <Typography variant="h4">{fNumber(react)}</Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            React
-          </Typography>
+          <Stack width={1} textAlign="center">
+            <Typography variant="h4">{fNumber(data.totalQty)}</Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Cart: Total Quantity
+            </Typography>
+          </Stack>
         </Stack>
-        <Stack width={1} textAlign="center">
-          <Typography variant="h4">{fNumber(react)}</Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            React
-          </Typography>
-        </Stack>
+      )}
+      <Stack sx={{ mt: 5, p: 5 }}>
+        {isSuccess && <CartCard data={data.food} />}
       </Stack>
     </Card>
   );
---
+++ dashboard/src/pages/users/consumer/components/openConsumer/Profile/CartCard.jsx
@@ -0,0 +1,82 @@
+import PropTypes from "prop-types";
+import { Icon } from "@iconify/react";
+import appleFilled from "@iconify/icons-ant-design/apple-filled";
+import percentage from "@iconify/icons-ant-design/percentage";
+import qtyIcon from "@iconify/icons-ant-design/fire-fill";
+import priceIcon from "@iconify/icons-ant-design/dollar";
+// material
+import { Box, Typography, Stack, styled } from "@mui/material";
+// utils
+import { fShortenNumber } from "../../../../../../utils/formatNumber";
+
+// ----------------------------------------------------------------------
+
+const ItemBlockStyle = styled((props) => (
+  <Stack direction="row" alignItems="center" {...props} />
+))({
+  minWidth: 72,
+  flex: "1 1",
+});
+
+const ItemIconStyle = styled(Icon)(({ theme }) => ({
+  width: 16,
+  height: 16,
+  marginRight: theme.spacing(0.5),
+  color: theme.palette.text.disabled,
+}));
+
+// ----------------------------------------------------------------------
+
+CartCard.propTypes = {
+  data: PropTypes.object,
+};
+
+function CardItem({ food }) {
+  return (
+    <Stack mb={2} direction="row" alignItems="center" spacing={2}>
+      <ItemBlockStyle sx={{ minWidth: 120 }}>
+        <Box
+          component="img"
+          alt={food.name}
+          src={food.photoUrl}
+          sx={{ height: 30, width: 30, mr: 2 }}
+        />
+        <Typography variant="subtitle2">{food.name}</Typography>
+      </ItemBlockStyle>
+      <ItemBlockStyle>
+        <ItemIconStyle icon={priceIcon} />
+        <Typography variant="body2">{fShortenNumber(food.price)}</Typography>
+      </ItemBlockStyle>
+      <ItemBlockStyle>
+        <ItemIconStyle icon={qtyIcon} />
+        <Typography variant="body2">{fShortenNumber(food.qty)}</Typography>
+      </ItemBlockStyle>
+      <ItemBlockStyle sx={{ minWidth: 88 }}>
+        <ItemIconStyle icon={appleFilled} />
+        <Typography variant="body2">{food.size}</Typography>
+      </ItemBlockStyle>
+      <ItemBlockStyle sx={{ minWidth: 88 }}>
+        <ItemIconStyle icon={percentage} />
+        <Typography variant="body2">
+          {fShortenNumber(food.discountRate)}
+        </Typography>
+      </ItemBlockStyle>
+    </Stack>
+  );
+}
+
+export default function CartCard({ data }) {
+  for (const property in data) {
+    console.log(`${property}: ${data[property]}`);
+    return (
+      <>
+        <Typography variant="h6">
+          {property === "notACombo" ? "" : "Combo"}
+        </Typography>
+        {data[property].map((food) => (
+          <CardItem key={food._id} food={food} />
+        ))}
+      </>
+    );
+  }
+}
