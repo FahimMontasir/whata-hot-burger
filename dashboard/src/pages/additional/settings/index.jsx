84f9a06a2354cd67d10f34a05f96cd5a6972452d
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-08-28 11:46:07 +0600

    add: terms and condition

--- dashboard/src/pages/additional/settings/index.jsx
+++ dashboard/src/pages/additional/settings/index.jsx
@@ -1,3 +1,44 @@
+// material
+import { Container } from "@mui/material";
+// component
+import HeaderBreadcrumbs from "../../../common/HeaderBreadcrumbs";
+import Page from "../../../common/Page";
+import useSettings from "../../../hooks/useSettings";
+// routes
+import { PATH_DASHBOARD } from "../../../routes/paths";
+import DialogModal from "../../../common/DialogModal";
+import { useGetTermsConditionQuery } from "../../../store/redux/api/termsAndCondition";
+import AddTerms from "./components/AddTerms";
+
 export default function SettingsPage() {
-  return <h1>theme stettings and terms and conditions</h1>;
+  const { themeStretch } = useSettings();
+  const { isSuccess, data } = useGetTermsConditionQuery();
+
+  return (
+    <Page title="Settings">
+      <Container maxWidth={themeStretch ? false : "lg"}>
+        <HeaderBreadcrumbs
+          heading="All Settings"
+          links={[
+            { name: "Dashboard", href: PATH_DASHBOARD.root },
+            { name: "Settings" },
+          ]}
+          action={
+            isSuccess && (
+              <DialogModal
+                title={data.object ? "Update Terms" : "Add Terms"}
+                fullScreen
+              >
+                <AddTerms
+                  currentValue={data.object}
+                  isEdit={Boolean(data.object)}
+                />
+              </DialogModal>
+            )
+          }
+        />
+        {/* <FAQList /> */}
+      </Container>
+    </Page>
+  );
 }
